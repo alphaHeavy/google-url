b761d8bb7df86c75e0d9ceb1d1d6d94de84675c3
Nathan Howell <nhowell@alphaheavy.com>
2013-01-23 17:17:07 -0800

    No temporary is required

--- cbits/wrapper.cpp
+++ cbits/wrapper.cpp
@@ -8,13 +8,10 @@ void parseUrl(const char *url, const int url_len, Result *res)
   Component scheme;
   if(!ExtractScheme(url, url_len, &scheme))
     return;
-  char* schemeString = (char*)malloc(scheme.len);
-  strncpy(const_cast<char*>(url)+scheme.begin,schemeString, scheme.len);
   url_parse::Parsed *result = new url_parse::Parsed();
 
-  if (strcmp(schemeString, "http"))
+  if (strncmp(&url[scheme.begin], "http", scheme.len) == 0)
   {
-    free(schemeString);
     res->urlType = STANDARD;
     ParseStandardURL(url,url_len,result);
     res->urlParsed = reinterpret_cast< ::Parsed*>(result);
